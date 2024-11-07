e9210bbabfbea8621261820b243ca14419607353
Falko Hilbert <fhilbert@viosys.com>
2024-11-07 17:03:41 +0100

    Add JWT authentication via LexikJWTAuthenticationBundle
    Integrated LexikJWTAuthenticationBundle for handling JWT authentication in the API. This includes configuration changes, updating `composer.json`, modifying security settings, and securing API routes. Adjusted the `VolunteerController` to support token-based authentication and added necessary JWT configurations in environment files.

--- src/Controller/Api/VolunteerController.php
+++ src/Controller/Api/VolunteerController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Volunteer;
 use App\Repository\VolunteerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/volunteers', name: 'app_volunteers_api_', format: 'json' )]
 class VolunteerController extends AbstractController
 {
     public function __construct(
-        private VolunteerRepository $volunteerRepository
+        private VolunteerRepository $volunteerRepository,
+        private SerializerInterface $serializer,
+        private ValidatorInterface $validator,
     )
     {
     }
@@ -23,5 +30,31 @@ class VolunteerController extends AbstractController
         return $this->json(
             $this->volunteerRepository->findAll(),
         );
+    }
+
+    #[Route('/{id}', name: 'get', methods: [Request::METHOD_GET])]
+    public function show(Volunteer $volunteer): JsonResponse
+    {
+        return $this->json($volunteer);
+    }
+
+    #[Route('/{id}', name: 'update', methods: [Request::METHOD_PUT])]
+    public function update(
+        Volunteer $volunteer,
+        Request $request
+    ): JsonResponse
+    {
+        $this->serializer->deserialize(
+            $request->getContent(), Volunteer::class, 'json',
+            [
+                AbstractNormalizer::OBJECT_TO_POPULATE => $volunteer
+            ]
+        );
+        $validationResult = $this->validator->validate($volunteer);
+        if( count($validationResult) !== 0) {
+            return $this->json($validationResult);
+        }
+        return $this->json($volunteer);
+
     }
 }
---
+++ src/Security/ApiAuthenticator.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class ApiAuthenticator extends AbstractAuthenticator
+{
+    public function supports(Request $request): ?bool
+    {
+        return $request->headers->has('X-Api-Key');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $apiKey = $request->headers->get('X-Api-Key');
+        if (!$apiKey) {
+            throw new CustomUserMessageAuthenticationException('No API key provided');
+        }
+        if( $apiKey !== 'secret' ) {
+            throw new CustomUserMessageAuthenticationException('Invalid API key');
+        }
+        return new SelfValidatingPassport(new UserBadge($apiKey, function ($apiKey) {
+            return $apiKey;
+        }));
+
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new JsonResponse(
+            [
+                'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+            ],
+            Response::HTTP_UNAUTHORIZED
+        );
+    }
+
+    //    public function start(Request $request, AuthenticationException $authException = null): Response
+    //    {
+    //        /*
+    //         * If you would like this class to control what happens when an anonymous user accesses a
+    //         * protected page (e.g. redirect to /login), uncomment this method and make this class
+    //         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+    //         *
+    //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+    //         */
+    //    }
+}
